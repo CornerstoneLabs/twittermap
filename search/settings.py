982089ae3b34288316f5a025c573827c957b12fd
Adam Auckland <adamauckland@gmail.com>
2016-09-25 17:22:45 +0100

    fixed settings

--- search/settings.py
+++ search/settings.py
@@ -1,3 +1,5 @@
+"""SETTINGS FILE."""
+from os.path import join, dirname
 from dotenv import load_dotenv
 import os
 
@@ -5,7 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 SEARCH = ['@kaelifa', ]
-CONSUMER_KEY=os.environ['CONSUMER_KEY']
-CONSUMER_SECRET=os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN=os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET=os.environ['ACCESS_TOKEN_SECRET']
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
