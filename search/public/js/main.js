c767db19584acd4d7f3133435ae1d8c78edaf922
Adam Auckland <adamauckland@gmail.com>
2016-09-25 17:13:01 +0100

    first pass

---
+++ search/public/js/main.js
@@ -0,0 +1,49 @@
+var url = '/data/tweets.json';
+
+(function () {
+	var map;
+	var ajaxRequest;
+	var plotlist;
+	var plotlayers=[];
+
+	function onDataLoaded (data) {
+		plotlist = data;
+
+		for (i=0;i<plotlist.length;i++) {
+			var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
+			var plotmark = new L.Marker(plotll);
+			plotmark.data=plotlist[i];
+			map.addLayer(plotmark);
+			plotmark.bindPopup("<h3><img src=\"" + plotlist[i].avatar + "\" />"+  plotlist[i].name+"</h3>"+plotlist[i].details);
+			plotlayers.push(plotmark);
+		}
+
+		var group = new L.featureGroup(plotlayers);
+
+		map.fitBounds(group.getBounds());
+	}
+
+	function getData () {
+		$.get(url, onDataLoaded);
+	}
+
+	function initmap() {
+		// set up the map
+		map = new L.Map('map');
+
+		// create the tile layer with correct attribution
+		var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+		var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+		var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 12, attribution: osmAttrib});
+
+		// start the map in South-East England
+		map.setView(new L.LatLng(51.3, 0.7),9);
+		map.addLayer(osm);
+
+		getData();
+	}
+
+	$(document).ready(function() {
+		initmap();
+	});
+})();
---
+++ search/search.py
@@ -0,0 +1,86 @@
+"""Search client."""
+
+from TwitterSearch import *
+import json
+import settings
+
+
+def read_towns():
+    """Read town data."""
+    town_file = open('public/data/towns.json', 'rt')
+    town_data = json.load(town_file)
+    town_file.close()
+
+    return town_data
+
+
+def lookup_town(towns, search):
+    """Look for a town."""
+    search = search.lower()
+
+    if search.find(',') != -1:
+        search = search.split(',')[0]
+
+    search = search.strip()
+
+    found = []
+
+    print('Looking up "%s"' % search)
+
+    for town in towns:
+        compare_name = town[5].lower().strip()
+        if compare_name == search:
+            found.append(town)
+
+        compare_name = town[6].lower().strip()
+        if compare_name == search:
+            found.append(town)
+
+    print('Found %s items' % (len(found)))
+
+    return found
+
+if __name__ == '__main__':
+    towns = read_towns()
+
+    output_data = []
+
+    try:
+        tso = TwitterSearchOrder()  # create a TwitterSearchOrder object
+        tso.set_keywords(settings.SEARCH)  # let's define all words we would like to have a look for
+        tso.set_language('en')  # we want to see German tweets only
+        tso.set_include_entities(False)  # and don't give us all those entity information
+
+        # it's about time to create a TwitterSearch object with our secret tokens
+        ts = TwitterSearch(
+            consumer_key=settings.CONSUMER_KEY,
+            consumer_secret=settings.CONSUMER_SECRET,
+            access_token=settings.ACCESS_TOKEN,
+            access_token_secret=settings.ACCESS_TOKEN_SECRET)
+
+        # this is where the fun actually starts :)
+        for tweet in ts.search_tweets_iterable(tso):
+            print('\n%s\n' % tweet)
+            print('@%s tweeted: %s %s ' % (tweet['user']['screen_name'], tweet['text'], tweet['user']['location']))
+            town_list = lookup_town(towns, tweet['user']['location'])
+
+            if len(town_list) > 0:
+                town = town_list[0]
+                lat = town[3]
+                lng = town[4]
+
+                output_obj = {}
+                output_obj['name'] = '@%s' % tweet['user']['screen_name']
+                output_obj['lon'] = lng
+                output_obj['lat'] = lat
+                output_obj['avatar'] = tweet['user']['profile_image_url']
+                output_obj['details'] = town[5]
+
+                output_data.append(output_obj)
+
+    except TwitterSearchException as e:  # take care of all those ugly errors if there are some
+        print(e)
+
+    output_file = open('public/data/tweets.json', 'wt')
+    output_file.write(json.dumps(output_data))
+    output_file.close()
