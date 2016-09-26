d8c969700def85518f2a93a5bb0e52944c612aa0
Adam Auckland <adamauckland@gmail.com>
2016-09-26 22:03:49 +0100

    added clustering

--- search/public/js/main.js
+++ search/public/js/main.js
@@ -1,10 +1,12 @@
-var url = '/data/tweets.json';
+var url = 'data/tweets.json';
 
 (function () {
 	var map;
 	var ajaxRequest;
 	var plotlist;
 	var plotlayers=[];
+	var markerGroup;
+	var firstScale = true;
 
 	function removeMarkers() {
 		for (i=0;i<plotlayers.length;i++) {
@@ -13,21 +15,40 @@ var url = '/data/tweets.json';
 		plotlayers=[];
 	}
 
-	function onDataLoaded (data) {
-		plotlist = data;
+	function countMarker(id) {
+		var count = plotlayers.filter(function (item) {
+			return item.data.id === id;
+		}).length;
 
-		for (i=0;i<plotlist.length;i++) {
-			var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
-			var plotmark = new L.Marker(plotll);
-			plotmark.data=plotlist[i];
-			map.addLayer(plotmark);
-			plotmark.bindPopup("<h3><img src=\"" + plotlist[i].avatar + "\" />"+  plotlist[i].name+"</h3>"+plotlist[i].details);
-			plotlayers.push(plotmark);
+		return count;
+	}
+
+	function fitBounds () {
+		var group = new L.featureGroup(plotlayers);
+		map.fitBounds(group.getBounds());
+	}
+
+	function onDataLoaded (data) {
+		for (i=0;i<data.length;i++) {
+			// check exists
+			if (countMarker(data[i].id) === 0) {
+				var plotll = new L.LatLng(data[i].lat,data[i].lon, true);
+
+				var newMarker = new L.Marker(plotll);
+				newMarker.data = data[i];
+
+				// add to group
+				markerGroup.addLayer(newMarker);
+
+				newMarker.bindPopup("<h3><img style=\"border-radius:5px\" src=\"" + data[i].avatar + "\" />"+  data[i].name+"</h3>"+data[i].details);
+				plotlayers.push(newMarker);
+			}
 		}
 
-		var group = new L.featureGroup(plotlayers);
-
-		map.fitBounds(group.getBounds());
+		if (firstScale === true) {
+			firstScale = false;
+			fitBounds();
+		}
 	}
 
 	function getData () {
@@ -44,12 +65,18 @@ var url = '/data/tweets.json';
 		var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 12, attribution: osmAttrib});
 
 		// start the map in South-East England
-		map.setView(new L.LatLng(51.3, 0.7),9);
+		map.setView(new L.LatLng(51.3, 0.7), 9);
 		map.addLayer(osm);
+
+		markerGroup = L.markerClusterGroup({
+			spiderfyOnMaxZoom: false,
+			showCoverageOnHover: false,
+			zoomToBoundsOnClick: true
+		});
+		map.addLayer(markerGroup);
 	}
 
 	function refresh () {
-		removeMarkers();
 		getData();
 	}
 
